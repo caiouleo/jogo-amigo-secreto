595cbfced7e8b1d61da354a8e4239eee270766ce
Caio Leonardo <caiouleo99@gmail.com>
2025-09-14 20:43:11 -0400

    Corrige função de sorteio e adiciona validação de entrada.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let Amigo = []
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById('amigo').value;
+
+    if (nomeAmigo.trim() !== "") { 
+        Amigo.push(nomeAmigo); 
+        document.getElementById('amigo').value = "";
+        console.log(Amigo);
+        mostrarLista();
+    } else {
+        alert("Por favor, insira um nome válido.");
+    }
+}
+
+function mostrarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+ Amigo.forEach(function(nome) {
+        let item = document.createElement("li"); 
+        item.textContent = nome; 
+        lista.appendChild(item); 
+    });
+}
+
+function sortearAmigo() {
+    if (Amigo.length === 0) {
+        alert("A lista de amigos está vazia!");
+        return;
+    }
+ let indiceAleatorio = Math.floor(Math.random() * Amigo.length);
+    let nomeSorteado = Amigo[indiceAleatorio];
+
+    alert("Amigo sorteado: " + nomeSorteado);
+}
+
+
